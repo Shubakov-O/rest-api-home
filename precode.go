d7d63fb37c5b29d24b3acc1cd9af7949a564a7a3
Oleg Shubakov <shybakov@list.ru>
2024-04-18 23:12:38 +0300

    number1

--- precode.go
+++ precode.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"net/http"
 
@@ -44,6 +46,17 @@ var tasks = map[string]Task{
 
 func main() {
 	r := chi.NewRouter()
+	// вывести список всех задач
+	r.Get("/tasks", getTasks)
+
+	// отправка задач
+	r.Post("/tasks", postTasks)
+
+	// вернуть задачу
+	r.Get("/tasks/{id}", getTask)
+
+	// удаления задачи
+	r.Delete("/tasks/{id}", delTask)
 
 	// здесь регистрируйте ваши обработчики
 	// ...
@@ -52,4 +65,74 @@ func main() {
 		fmt.Printf("Ошибка при запуске сервера: %s", err.Error())
 		return
 	}
+}
+
+// Обработчик удаления задачи по ID
+func delTask(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+
+	_, ok := tasks[id]
+	if !ok {
+		http.Error(w, "Задача не найдена", http.StatusBadRequest)
+		return
+	}
+
+	delete(tasks, id)
+	w.Header().Set("Countent-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+}
+
+// Обработчик для получения задачи по ID
+func getTask(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	task, ok := tasks[id]
+	if !ok {
+		http.Error(w, "Задача не найдена", http.StatusNoContent)
+		return
+	}
+
+	resp, err := json.Marshal(task)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(resp)
+
+}
+
+// Обработчик для отправки задачи на сервер
+func postTasks(w http.ResponseWriter, r *http.Request) {
+	var task Task
+	var buf bytes.Buffer
+
+	_, err := buf.ReadFrom(r.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if err = json.Unmarshal(buf.Bytes(), &task); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	tasks[task.ID] = task
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+}
+
+// Обработчик для получения всех задач
+func getTasks(w http.ResponseWriter, r *http.Request) {
+	resp, err := json.Marshal(tasks)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Countent-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(resp)
 }
